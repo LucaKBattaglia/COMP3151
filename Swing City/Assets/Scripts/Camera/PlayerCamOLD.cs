2d626d5adda1dfcc6a404347a25fbcf15e415852
Will <william.dixon@students.mq.edu.au>
2024-08-04 20:40:57 +1000

    Added sliding + wall jumping

--- Swing City/Assets/Scripts/Camera/PlayerCamOLD.cs
+++ Swing City/Assets/Scripts/Camera/PlayerCamOLD.cs
@@ -1,16 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using DG.Tweening;
 
-public class PlayerCam : MonoBehaviour
+public class PlayerCamOLD : MonoBehaviour
 {
     public float sensX;
     public float sensY;
-
     public Transform orientation;
-    public Transform camHolder;
-
     float xRotation;
     float yRotation;
 
@@ -32,17 +28,7 @@ public class PlayerCam : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         // rotate cam and orientation
-        camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-    }
-
-    public void DoFov(float endValue)
-    {
-        //GetComponent<Camera>().DOFieldOfView(endValue, 0.25f);
-    }
-
-    public void DoTilt(float zTilt)
-    {
-        //transform.DOLocalRotate(new Vector3(0, 0, zTilt), 0.25f);
     }
 }
