f5ddc29f778ab42ffaf8c6ab19e882b5887d3549
Luca <gianluca.battaglia1@students.mq.edu.au>
2024-08-09 16:45:52 +1000

    Squashed commit of the following:
    commit 0898303e8603f762db811614b499e55f23495b71
    Author: Luca <gianluca.battaglia1@students.mq.edu.au>
    Date:   Fri Aug 9 16:37:18 2024 +1000
    
        Adding UI Manager into Scenes
    
        + Added UI Manager into PitchScene
        + Added UI Manager into PitchScene2
        (does not freeze camera in Pitchscene2 properly due to the scene not using a cameraUpdater)
    
    commit bbbc0583ce3b1eea30b36ad4018a540a38c9bef3
    Author: Luca <gianluca.battaglia1@students.mq.edu.au>
    Date:   Fri Aug 9 16:26:49 2024 +1000
    
        Art&Models Folder Update
    
        + Adding all files from Art&Models from Branch "Luca"
    
    commit 51ad0e1d5b817553f629c1662065b215e15f70c3
    Merge: 3316cd4 6d090e1
    Author: Luca <gianluca.battaglia1@students.mq.edu.au>
    Date:   Fri Aug 9 16:25:08 2024 +1000
    
        Merge branch 'Luca'
    
    commit 3316cd41a3d474c9867ed1289f61b59b20646fe0
    Merge: 2d626d5 3a61ba5
    Author: Dylan W <dylan.wijesinghe@students.mq.edu.au>
    Date:   Thu Aug 8 13:55:16 2024 +1000
    
        Merge branch 'Dylan'
    
    commit 3a61ba5ee9a02ea6646bc32e09d857cbf374e1d3
    Author: Dylan W <dylan.wijesinghe@students.mq.edu.au>
    Date:   Thu Aug 8 13:54:37 2024 +1000
    
        Player - FPS Grappling
    
        - FPS Controller from asset store imported
        - Grappling Gun and Wall Running now uses the FPS Controller system for improved grappling
        - New scene added: PitchScene2
    
    commit 2d626d5adda1dfcc6a404347a25fbcf15e415852
    Author: Will <william.dixon@students.mq.edu.au>
    Date:   Sun Aug 4 20:40:57 2024 +1000
    
        Added sliding + wall jumping
    
    commit 59c5be01293c16cf11fb6ceefa119c68bb6bce5f
    Author: Will <william.dixon@students.mq.edu.au>
    Date:   Sun Aug 4 20:01:58 2024 +1000
    
        Added wall running
    
    commit b0bed18460da1e2509b357e735fd6cfde618f55b
    Merge: 1abce5e 46adfe1
    Author: Dylan W <dylan.wijesinghe@students.mq.edu.au>
    Date:   Sun Aug 4 19:13:04 2024 +1000
    
        Merge branch 'Dylan'
    
    commit 1abce5e95f48c29b31cc72de0b1c4a707fba2da6
    Author: Zion-XiaoxiSu-OGLocGo <jnsuxx0320@gmail.com>
    Date:   Sun Aug 4 18:30:37 2024 +1000
    
        pitch scene, map concept
    
    commit 3c9d0873baba33edb1c8ee90fd494e8d1e846489
    Author: Zion-XiaoxiSu-OGLocGo <jnsuxx0320@gmail.com>
    Date:   Sun Aug 4 18:02:51 2024 +1000
    
        prefab folder, 16:9 aspect
    
    commit 46adfe10af2bc068e0a5a7c1e0c5e0f299343aa2
    Author: Dylan W <dylan.wijesinghe@students.mq.edu.au>
    Date:   Sat Aug 3 14:48:06 2024 +1000
    
        Player - Advanced Grappling
    
        - Drastically changed the grappling system to implement jump velocities to the grappling points
        - Added more setting values to grappling
        - Removed traces of SpringJoints
        - Input key can now be configured for grappling
        - PlayerMovement now makes partial use of grappling when calculating velocity and its rigidbody

---
+++ Swing City/Assets/Scripts/Movement/OLD/PlayerMovementOLD.cs
@@ -0,0 +1,238 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerMovementOLD : MonoBehaviour
+{
+//------------------------------------------------------------------------------------------//
+// Start of Paramaters Section
+// The script is split up into seperate sections to help improve readability and identifying what section is in charge of what mechanics / functions.
+//------------------------------------------------------------------------------------------//
+    [Header("Player")] // Player RigidBody/Body
+    Rigidbody rb;
+
+    public enum MovementState { 
+        walking,
+        sprinting,
+        wallrunning,
+        crouching,
+        sliding,
+        air
+    }
+    public MovementState state;
+
+    //------------------------------------------------------------------------------------------//
+    [Header("Movement")] // Movement speeds & direction 
+    public float moveSpeed;
+    private float setMoveSpeed;
+    Vector3 moveDirection;
+    public float walkSpeed;
+    public float sprintSpeed;
+    public Transform orientation;    
+    public float groundDrag; // Drag | Air-Resistance when on ground | Friction
+    public bool isFrozen;
+    public bool activeGrapple;
+    public float wallrunSpeed;
+
+    public bool sliding;
+    public bool crouching;
+    public bool wallrunning;
+//------------------------------------------------------------------------------------------//
+    [Header("Keybinds")] // Keybinds & player controls
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+//------------------------------------------------------------------------------------------//
+    [Header("Keybinds")] // Keybinds & Player Input
+    public KeyCode jumpKey = KeyCode.Space; // Jump button 
+    float horizontalInput;
+    float verticalInput;
+//------------------------------------------------------------------------------------------//
+    [Header("Ground Check")] // Controls: What is Ground, what surface is solid & am l currently on ground?
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+    public TextMeshProUGUI text_speed;
+    public TextMeshProUGUI text_mode;
+//------------------------------------------------------------------------------------------//
+// End of Paramaters Section
+//------------------------------------------------------------------------------------------//
+
+//------------------------------------------------------------------------------------------//
+// Start of Functions Section
+//------------------------------------------------------------------------------------------//
+
+    //--------------------------------------------------------------------------------------//
+    // Function Start & Update Section
+    //--------------------------------------------------------------------------------------//
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        readyToJump = true;
+
+        setMoveSpeed = moveSpeed;
+    }
+
+    private void Update()
+    {
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        // handle drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    //--------------------------------------------------------------------------------------//
+    // Player Controls & Input Section
+    //--------------------------------------------------------------------------------------//
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // can player currently jump
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false; // makes it so player cannot jump mid jump
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    //--------------------------------------------------------------------------------------//
+    // Player Speed & Drag Section
+    //--------------------------------------------------------------------------------------//
+    private void MovePlayer()
+    {
+        if (activeGrapple) return;
+
+        // calculate player movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        // reset player movement speed
+        moveSpeed = setMoveSpeed;
+
+        // if player is frozen, stop them in place
+        if (isFrozen)
+        {
+            moveSpeed = 0;
+            rb.velocity = Vector3.zero;
+        }
+        
+        // when player is on ground
+        else if(grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // when player is in air
+        else if(!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+    private void SpeedControl()
+    {
+        if (activeGrapple) return;
+        
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+
+        //text_speed.SetText("Speed: " + flatVel.magnitude);
+    }
+
+    //--------------------------------------------------------------------------------------//
+    // Jump Ability Section
+    //--------------------------------------------------------------------------------------//
+    private void Jump()
+    {
+        // reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    //--------------------------------------------------------------------------------------//
+    // Grappling Section
+    //--------------------------------------------------------------------------------------//
+    // variable for movement detection
+    private bool enableMovementOnNextTouch;
+    public void JumpToPosition(Vector3 targetPosition, float trajectoryHeight)
+    {
+        activeGrapple = true;
+
+        // use jumping velocity when player pulls the grapple to the point
+        velocityToSet = CalculateJumpVelocity(transform.position, targetPosition, trajectoryHeight);
+        Invoke(nameof(SetVelocity), 0.1f);
+
+        // restore player controls after set duration
+        Invoke(nameof(ResetRestrictions), GameObject.Find("grappleGun").GetComponent<GrapplingGun>().grappleDuration);
+    }
+
+    // set velocity to rigidbody
+    private Vector3 velocityToSet;
+    private void SetVelocity()
+    {
+        enableMovementOnNextTouch = true;
+        rb.velocity = velocityToSet;
+    }
+
+    public void ResetRestrictions()
+    {
+        activeGrapple = false;
+    }
+
+    // reset player controls when colliding
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (enableMovementOnNextTouch)
+        {
+            enableMovementOnNextTouch = false;
+            ResetRestrictions();
+
+            GameObject.Find("grappleGun").GetComponent<GrapplingGun>().StopGrapple();
+        }
+    }
+
+    // main math method for calculating jump velocity
+    public Vector3 CalculateJumpVelocity(Vector3 startPoint, Vector3 endPoint, float trajectoryHeight)
+    {
+        float gravity = Physics.gravity.y;
+        float displacementY = endPoint.y - startPoint.y;
+        Vector3 displacementXZ = new Vector3(endPoint.x - startPoint.x, 0f, endPoint.z - startPoint.z);
+
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * trajectoryHeight);
+        Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * trajectoryHeight / gravity) 
+            + Mathf.Sqrt(2 * (displacementY - trajectoryHeight) / gravity));
+
+        return velocityXZ + velocityY;
+    }
+}
+//------------------------------------------------------------------------------------------//
+// End of Functions Section
+//------------------------------------------------------------------------------------------//
+
--- Swing City/Assets/Scripts/Movement/PlayerMovement.cs
+++ Swing City/Assets/Scripts/Movement/PlayerMovement.cs
@@ -5,54 +5,73 @@ using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
-//------------------------------------------------------------------------------------------//
-// Start of Paramaters Section
-// The script is split up into seperate sections to help improve readability and identifying what section is in charge of what mechanics / functions.
-//------------------------------------------------------------------------------------------//
-    [Header("Player")] // Player RigidBody/Body
-    Rigidbody rb;
-//------------------------------------------------------------------------------------------//
-    [Header("Movement")] // Movement speeds & direction 
-    public float moveSpeed;
-    Vector3 moveDirection;
-    [HideInInspector] public float walkSpeed;
-    [HideInInspector] public float sprintSpeed;
-    public Transform orientation;    
-    public float groundDrag; // Drag | Air-Resistance when on ground | Friction
-//------------------------------------------------------------------------------------------//
-    [Header("Keybinds")] // Keybinds & player controls
+    [Header("Movement")]
+    public bool isFrozen;
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
+    public float wallrunSpeed;
+    public bool activeGrapple;
+    public float groundDrag;
+
+    [Header("Jumping")]
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
     bool readyToJump;
-//------------------------------------------------------------------------------------------//
-    [Header("Keybinds")] // Keybinds & Player Input
-    public KeyCode jumpKey = KeyCode.Space; // Jump button 
-    float horizontalInput;
-    float verticalInput;
-//------------------------------------------------------------------------------------------//
-    [Header("Ground Check")] // Controls: What is Ground, what surface is solid & am l currently on ground?
+
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
+
+    [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
     bool grounded;
-    [HideInInspector] public TextMeshProUGUI text_speed;
-//------------------------------------------------------------------------------------------//
-// End of Paramaters Section
-//------------------------------------------------------------------------------------------//
 
-//------------------------------------------------------------------------------------------//
-// Start of Functions Section
-//------------------------------------------------------------------------------------------//
+    [Header("Slope Handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    private bool exitingSlope;
+    
 
-    //--------------------------------------------------------------------------------------//
-    // Function Start & Update Section
-    //--------------------------------------------------------------------------------------//
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        wallrunning,
+        crouching,
+        air
+    }
+
+    public bool sliding;
+    public bool crouching;
+    public bool wallrunning;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
 
         readyToJump = true;
+
+        startYScale = transform.localScale.y;
     }
 
     private void Update()
@@ -62,6 +81,7 @@ public class PlayerMovement : MonoBehaviour
 
         MyInput();
         SpeedControl();
+        StateHandler();
 
         // handle drag
         if (grounded)
@@ -75,72 +95,207 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer();
     }
 
-    //--------------------------------------------------------------------------------------//
-    // Player Controls & Input Section
-    //--------------------------------------------------------------------------------------//
     private void MyInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        // can player currently jump
+        // when to jump
         if(Input.GetKey(jumpKey) && readyToJump && grounded)
         {
-            readyToJump = false; // makes it so player cannot jump mid jump
+            readyToJump = false;
 
             Jump();
 
             Invoke(nameof(ResetJump), jumpCooldown);
         }
-    }
 
-    //--------------------------------------------------------------------------------------//
-    // Player Speed & Drag Section
-    //--------------------------------------------------------------------------------------//
-    private void MovePlayer()
-    {
-        // calculate player movement direction
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        // when player is on ground
-        if(grounded)
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-
-        // when player is in air
-        else if(!grounded)
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
-    }
-    private void SpeedControl()
-    {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        // limit velocity if needed
-        if(flatVel.magnitude > moveSpeed)
+        // start crouch
+        if (Input.GetKeyDown(crouchKey))
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
         }
 
-        //text_speed.SetText("Speed: " + flatVel.magnitude);
+        // stop crouch
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
     }
 
-    //--------------------------------------------------------------------------------------//
-    // Jump Ability Section
-    //--------------------------------------------------------------------------------------//
+    private void StateHandler()
+    {
+        // Wallrunning
+        if (wallrunning)
+        {
+            state = MovementState.wallrunning;
+            moveSpeed = wallrunSpeed;
+        }
+
+        // Mode - Crouching
+        if (Input.GetKey(crouchKey))
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+        }
+
+        // Mode - Sprinting
+        else if(grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+
+        // Mode - Walking
+        else if (grounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+
+        // Mode - Air
+        else
+        {
+            state = MovementState.air;
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        // on slope
+        if (OnSlope() && !exitingSlope)
+        {
+            rb.AddForce(GetSlopeMoveDirection(moveDirection) * moveSpeed * 20f, ForceMode.Force);
+
+            if (rb.velocity.y > 0)
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+        }
+
+        // on ground
+        else if(grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // in air
+        else if(!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+
+        // turn gravity off while on slope
+        rb.useGravity = !OnSlope();
+    }
+
+    private void SpeedControl()
+    {
+        // limiting speed on slope
+        if (OnSlope() && !exitingSlope)
+        {
+            if (rb.velocity.magnitude > moveSpeed)
+                rb.velocity = rb.velocity.normalized * moveSpeed;
+        }
+
+        // limiting speed on ground or in air
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+            // limit velocity if needed
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            }
+        }
+    }
+
     private void Jump()
     {
+        exitingSlope = true;
+
         // reset y velocity
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
-
     private void ResetJump()
     {
         readyToJump = true;
-    }
-}
-//------------------------------------------------------------------------------------------//
-// End of Functions Section
-//------------------------------------------------------------------------------------------//
 
+        exitingSlope = false;
+    }
+
+    public bool OnSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+
+        return false;
+    }
+
+    public Vector3 GetSlopeMoveDirection(Vector3 dir)
+    {
+        return Vector3.ProjectOnPlane(dir, slopeHit.normal).normalized;
+    }
+
+    //--------------------------------------------------------------------------------------//
+    // Grappling Section
+    //--------------------------------------------------------------------------------------//
+    // variable for movement detection
+    private bool enableMovementOnNextTouch;
+    public void JumpToPosition(Vector3 targetPosition, float trajectoryHeight)
+    {
+        activeGrapple = true;
+
+        // use jumping velocity when player pulls the grapple to the point
+        velocityToSet = CalculateJumpVelocity(transform.position, targetPosition, trajectoryHeight);
+        Invoke(nameof(SetVelocity), 0.1f);
+
+        // restore player controls after set duration
+        Invoke(nameof(ResetRestrictions), GameObject.Find("grappleGun").GetComponent<GrapplingGun>().grappleDuration);
+    }
+
+    // set velocity to rigidbody
+    private Vector3 velocityToSet;
+    private void SetVelocity()
+    {
+        enableMovementOnNextTouch = true;
+        rb.velocity = velocityToSet;
+    }
+
+    public void ResetRestrictions()
+    {
+        activeGrapple = false;
+    }
+
+    // reset player controls when colliding
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (enableMovementOnNextTouch)
+        {
+            enableMovementOnNextTouch = false;
+            ResetRestrictions();
+
+            GameObject.Find("grappleGun").GetComponent<GrapplingGun>().StopGrapple();
+        }
+    }
+
+    // main math method for calculating jump velocity
+    public Vector3 CalculateJumpVelocity(Vector3 startPoint, Vector3 endPoint, float trajectoryHeight)
+    {
+        float gravity = Physics.gravity.y;
+        float displacementY = endPoint.y - startPoint.y;
+        Vector3 displacementXZ = new Vector3(endPoint.x - startPoint.x, 0f, endPoint.z - startPoint.z);
+
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * trajectoryHeight);
+        Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * trajectoryHeight / gravity)
+            + Mathf.Sqrt(2 * (displacementY - trajectoryHeight) / gravity));
+
+        return velocityXZ + velocityY;
+    }
+
+}
