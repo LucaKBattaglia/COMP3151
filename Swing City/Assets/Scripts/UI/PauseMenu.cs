5dbefb1062a2dd2fc7bfd49c1d30b73d63b9f493
Luca Battaglia <gianluca.battaglia1@students.mq.edu.au>
2024-08-09 13:01:49 +1000

    UI - PauseMenu script
    Adds Script for the PauseMenu
    
    + PasueMenu.cs script

---
+++ Swing City/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject PauseMenuCanvas;
+    void Start(){
+     Time.timeScale = 1f;   
+    }
+    void Update(){
+     if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Paused){
+                Play();
+            }
+
+            else {
+                Stop();
+            }
+        }   
+    }
+
+    void Stop(){
+        PauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        Paused = true;
+        Cursor.visible = true;
+    }
+    public void Play(){
+        PauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+        Cursor.visible = false;
+    }
+
+    public void MainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void BackButton(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+
+
+}
